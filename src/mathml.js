e66d90c404495fd38fbf01eb389fdc617e13c23f
x-at-01 <x-at-01@googlegroups.com>
2026-06-26 16:03:46 +0800

    opt: remove MENCLOSE style default parameter to save bytes

--- src/mathml.js
+++ src/mathml.js
@@ -71,8 +71,8 @@ const MROW = "mrow",
     [TYPE_MROOT]: ([, n_1, n_2]) => nest("mroot", n_1, n_2),
     [TYPE_LEFT_RIGHT]: ([, ns]) => wrap(MROW, ns.map(show).join("")),
     [TYPE_OVERLINE]: ([, n_1]) => nest("mover", n_1, [TYPE_OP, "¯"]),
-    [TYPE_MENCLOSE]: ([, style_id, node], style = STYLES[style_id]) =>
-      style ? wrap("mrow", row(node), style) : row(node),
+    [TYPE_MENCLOSE]: ([, style_id, node]) =>
+      STYLES[style_id] ? wrap("mrow", row(node), STYLES[style_id]) : row(node),
     [TYPE_MPHANTOM]: ([, n_1]) => wrap("mphantom", row(n_1)),
     [TYPE_MATRIX]: (n) => {
       const [, env, rows] = n,
